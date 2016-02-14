e3c677e169d3333b4521b378c912213b947a357f
NumiLarsson <ante9311@gmail.com>
2016-02-14 15:22:01 +0100

    I think I fixed it!

--- Operating Systems/inlup2/craps/common.h
+++ Operating Systems/inlup2/craps/common.h
@@ -14,7 +14,7 @@
 #include <signal.h>
 #include <unistd.h>
 
-#define NUM_PLAYERS 3
+#define NUM_PLAYERS 6
 
 void shooter(int pid, int seed_fd_rd, int score_fd_write);
 void waitstat(pid_t pid, int status);
